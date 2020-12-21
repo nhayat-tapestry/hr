02868c268579e883a3aee141ec323375cbab58b9
nhayat-tapestry <75479617+nhayat-tapestry@users.noreply.github.com>
2020-12-21 20:00:09 +0500

    Update buildbar.sh

--- buildbar.sh
+++ buildbar.sh
@@ -1,4 +1,4 @@
 echo 'building bar file'
 cd /opt/ace-11.0.0.9/tools
 
- . /opt/ace-11.0.0.9/server/bin/mqsiprofile && mqsicreatebar -data $WORKSPACE -b $WORKSPACE/${BUILD_ID}.bar  -a AREACODE_SEARCH_AR -compileOnly -v createbartrace.txt -cleanBuild 
+ . /opt/ace-11.0.0.9/server/bin/mqsiprofile && mqsicreatebar -data $WORKSPACE/PI -b $WORKSPACE/${BUILD_ID}.bar  -a AREACODE_SEARCH_AR -compileOnly -v createbartrace.txt -cleanBuild 
