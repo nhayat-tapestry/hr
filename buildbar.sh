e926d5abdd18028bebd49b72c79869143dcbf599
nhayat-tapestry <75479617+nhayat-tapestry@users.noreply.github.com>
2020-12-23 15:53:05 +0500

    Update buildbar.sh

--- buildbar.sh
+++ buildbar.sh
@@ -4,7 +4,7 @@ echo 'building bar file'
 cd /opt/ace-11.0.0.9/tools
 
 #test="${params.Applications_Name}"
-echo "params.Applications_Name"
+echo $params.Applications_Name
 #appsbuil="$(echo $test| tr ',' ' ')"
 #test5=$(echo $test| tr ',' ' ')
 
