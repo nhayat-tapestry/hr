ef4ed648fe8c6aa3bccade4fcd787a74f4d5328d
nhayat-tapestry <75479617+nhayat-tapestry@users.noreply.github.com>
2020-12-28 19:32:24 +0500

    Update buildbar.sh

--- buildbar.sh
+++ buildbar.sh
@@ -9,8 +9,9 @@ test=$1
 
 #"echo ${params.Applications_Name}"
 echo ${test}
-#appsbuil="$(echo $test| tr ',' ' ')"
+appsbuild="$(echo $test| tr ',' ' ')"
 #test5=$(echo $test| tr ',' ' ')
+echo ${appsbuild}
 
 
 # . /opt/ace-11.0.0.9/server/bin/mqsiprofile && mqsicreatebar -data $WORKSPACE/PI -b $WORKSPACE/Misc/BARfiles/${BUILD_ID}.bar  -a AREACODE_SEARCH_AR HRIS_ESB_PULL_DATA HRIS_ESB_TWILIO_CREATE -compileOnly -v createbartrace.txt -cleanBuild 
