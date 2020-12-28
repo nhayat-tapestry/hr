81d920bdf0432f8a7f4b5086368df7bff5c12eea
nhayat-tapestry <75479617+nhayat-tapestry@users.noreply.github.com>
2020-12-28 20:06:50 +0500

    Update buildbar.sh

--- buildbar.sh
+++ buildbar.sh
@@ -14,7 +14,20 @@ appsbuild="$(echo $test| tr ',' ' ')"
 #test5=$(echo $test| tr ',' ' ')
 echo ${appsbuild}
 
+if [ -z "$test2" ]
+then
+     
+    . /opt/ace-11.0.0.9/server/bin/mqsiprofile && mqsicreatebar -data $WORKSPACE/PI -b $WORKSPACE/Misc/BARfiles/${BUILD_ID}.bar  -a ${appsbuild}  -deployAsSource -v createbartrace.txt -cleanBuild 
+
+      #echo "\$my_var is NULL"
+else
+
+   . /opt/ace-11.0.0.9/server/bin/mqsiprofile && mqsicreatebar -data $WORKSPACE/PI -b $WORKSPACE/Misc/BARfiles/${BUILD_ID}.bar  -a ${appsbuild} -l ${test2} -deployAsSource -v createbartrace.txt -cleanBuild 
+
+    #  echo "\$my_var is NOT NULL"
+fi
+
 
 # . /opt/ace-11.0.0.9/server/bin/mqsiprofile && mqsicreatebar -data $WORKSPACE/PI -b $WORKSPACE/Misc/BARfiles/${BUILD_ID}.bar  -a AREACODE_SEARCH_AR HRIS_ESB_PULL_DATA HRIS_ESB_TWILIO_CREATE -compileOnly -v createbartrace.txt -cleanBuild 
 # . /opt/ace-11.0.0.9/server/bin/mqsiprofile && mqsicreatebar -data $WORKSPACE/PI -b $WORKSPACE/Misc/BARfiles/${BUILD_ID}.bar  -a ESB_IDM_EMPLOYEE_FEED PURCHSE_PHONENUMBER_AR -l ESB_IDMInboundWebService_MSPLIB  -compileOnly -v createbartrace.txt -cleanBuild 
- . /opt/ace-11.0.0.9/server/bin/mqsiprofile && mqsicreatebar -data $WORKSPACE/PI -b $WORKSPACE/Misc/BARfiles/${BUILD_ID}.bar  -a ${appsbuild}  -deployAsSource -v createbartrace.txt -cleanBuild 
+ #. /opt/ace-11.0.0.9/server/bin/mqsiprofile && mqsicreatebar -data $WORKSPACE/PI -b $WORKSPACE/Misc/BARfiles/${BUILD_ID}.bar  -a ${appsbuild}  -deployAsSource -v createbartrace.txt -cleanBuild 
