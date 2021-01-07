cb6d161a3f8c64de11a7f6803e7ebea34f9ff0ce
nhayat-tapestry <75479617+nhayat-tapestry@users.noreply.github.com>
2021-01-07 23:41:23 +0500

    Update buildbar.sh

--- buildbar.sh
+++ buildbar.sh
@@ -31,7 +31,7 @@ git add . -A
 git commit -m "hi"
 #git push https://github.com/nhayat-tapestry/hr.git
 
-git remote add origin "https://github.com/nhayat-tapestry/hr.git"
+#git remote add origin "https://github.com/nhayat-tapestry/hr.git"
 git push -u origin master
 
 # . /opt/ace-11.0.0.9/server/bin/mqsiprofile && mqsicreatebar -data $WORKSPACE/PI -b $WORKSPACE/Misc/BARfiles/${BUILD_ID}.bar  -a AREACODE_SEARCH_AR HRIS_ESB_PULL_DATA HRIS_ESB_TWILIO_CREATE -compileOnly -v createbartrace.txt -cleanBuild 
