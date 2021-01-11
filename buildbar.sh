650d426c7d442cdbc19823019ca09d771bd53d6d
nhayat-tapestry <75479617+nhayat-tapestry@users.noreply.github.com>
2021-01-11 13:53:51 +0500

    Update buildbar.sh

--- buildbar.sh
+++ buildbar.sh
@@ -28,8 +28,10 @@ else
 fi
 cd /$WORKSPACE/
 git add . -A
-
-git commit --author="nhayat-tapestry <>" -m "hi" $WORKSPACE/Misc/BARfiles/${BUILD_ID}.bar
+git config --global user.name "nhayat-tapestry"
+git config --global user.email "nhayat@tapestry.com"
+git commit --author="nhayat-tapestry <nhayat@tapestry.com>" -m "hi" $WORKSPACE/Misc/BARfiles/${BUILD_ID}.bar
+#git commit --author="nhayat-tapestry <>" -m "hi" $WORKSPACE/Misc/BARfiles/${BUILD_ID}.bar
 #git commit --author="Name <>" -m "whatever"
 #git push https://github.com/nhayat-tapestry/hr.git
 #git remote set-url origin "https://github.com/nhayat-tapestry/hr.git"
