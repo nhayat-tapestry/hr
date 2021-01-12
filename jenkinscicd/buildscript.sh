afed63922fee21558b1e4d73896cb6dee7ee8655
nhayat-tapestry <75479617+nhayat-tapestry@users.noreply.github.com>
2021-01-12 13:40:08 +0500

    Update buildscript.sh

--- jenkinscicd/buildscript.sh
+++ jenkinscicd/buildscript.sh
@@ -1,1 +1,57 @@
+#!/bin/bash
+#!/usr/bin/env sh
+
+echo 'building bar file'
+cd /opt/ace-11.0.0.9/tools
+
+test=$1
+test2=$2
+# echo "$chosen_app"
+
+#"echo ${params.Applications_Name}"
+echo ${test2}
+appsbuild="$(echo $test| tr ',' ' ')"
+#test5=$(echo $test| tr ',' ' ')
+echo ${appsbuild}
+
+if [ -z "$test2" ]
+then
+     
+    . /opt/ace-11.0.0.9/server/bin/mqsiprofile && mqsicreatebar -data $WORKSPACE/PI -b $WORKSPACE/Misc/BARfiles/${BUILD_ID}.bar  -a ${appsbuild}  -deployAsSource -v createbartrace.txt -cleanBuild 
+
+      #echo "\$my_var is NULL"
+else
+
+   . /opt/ace-11.0.0.9/server/bin/mqsiprofile && mqsicreatebar -data $WORKSPACE/PI -b $WORKSPACE/Misc/BARfiles/${BUILD_ID}.bar  -a ${appsbuild} -l ${test2} -deployAsSource -v createbartrace.txt -cleanBuild 
+
+    #  echo "\$my_var is NOT NULL"
+fi
+cd /$WORKSPACE/
+#git init
+git add . -A
+##git config --global user.name "nhayat-tapestry"
+##git config --global user.email "nhayat@tapestry.com"
+git config --global --unset https.proxy
+git remote rm origin
+git remote add origin "https://nhayat-tapestry:09f738f07d7eda4f8a5157fc1591d51920fa48b2@github.com/nhayat-tapestry/hr.git"
+git commit --amend --reset-author -m "hi" $WORKSPACE/Misc/BARfiles/${BUILD_ID}.bar
+#git commit -m "hi" $WORKSPACE/Misc/BARfiles/${BUIL:D_ID}.bar
+#git commit --author="nhayat-tapestry <>" -m "hi" $WORKSPACE/Misc/BARfiles/${BUILD_ID}.bar
+#git commit --author="Name <>" -m "whatever"
+#git push https://github.com/nhayat-tapestry/hr.git
+#git remote set-url origin git@github.com:username/repository
+#git remote set-url origin "git@github.com:nhayat-tapestry/hr.git"
+#git remote set-url origin git@github.com:nhayat-tapestry/hr.git
+#git remote add origin "https://github.com/nhayat-tapestry/hr.git"
+#git remote set-url origin "https://nhayat-tapestry:13b89db28090e683d1d238681112d28cf944d712@github.com/nhayat-tapestry/hr.git"
+##git remote set-url origin "https://nhayat-tapestry:b969d179539adb50131c1ad50eef1297351b00b1@github.com/nhayat-tapestry/hr.git"
+#git push origin master
+  #git config --global push.default matching
+#git push https://13b89db28090e683d1d238681112d28cf944d712@github.com/nhayat-tapestry/hr.git
+git push origin master
+
+
+# . /opt/ace-11.0.0.9/server/bin/mqsiprofile && mqsicreatebar -data $WORKSPACE/PI -b $WORKSPACE/Misc/BARfiles/${BUILD_ID}.bar  -a AREACODE_SEARCH_AR HRIS_ESB_PULL_DATA HRIS_ESB_TWILIO_CREATE -compileOnly -v createbartrace.txt -cleanBuild 
+# . /opt/ace-11.0.0.9/server/bin/mqsiprofile && mqsicreatebar -data $WORKSPACE/PI -b $WORKSPACE/Misc/BARfiles/${BUILD_ID}.bar  -a ESB_IDM_EMPLOYEE_FEED PURCHSE_PHONENUMBER_AR -l ESB_IDMInboundWebService_MSPLIB  -compileOnly -v createbartrace.txt -cleanBuild 
+ #. /opt/ace-11.0.0.9/server/bin/mqsiprofile && mqsicreatebar -data $WORKSPACE/PI -b $WORKSPACE/Misc/BARfiles/${BUILD_ID}.bar  -a ${appsbuild}  -deployAsSource -v createbartrace.txt -cleanBuild 
 
